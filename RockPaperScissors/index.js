585456daf2e78ea684cdeac063f488e4e4d7ba6e
Sajid <sajidahamed2001@gmail.com>
2024-09-28 13:20:10 +0530

    all functionality working

--- RockPaperScissors/index.js
+++ RockPaperScissors/index.js
@@ -1,102 +1,57 @@
-
 let humanChoice,humanScore = 0,compChoice,compScore = 0,draw = 0;
 
-function playGame() {
-    
-   
-    playRound(humanChoice,compChoice)
-    playRound(humanChoice,compChoice)
-    playRound(humanChoice,compChoice)
-    playRound(humanChoice,compChoice)
-    playRound(humanChoice,compChoice)
-    displayScore(humanScore,compScore,draw)
-}
-
-playGame()
-
-function getCompChoice (){
-    let rand = Math.random();
-    if (rand > 0 && rand < 0.33){
-        compChoice = 'rock'
-        //return console.log(compChoice)
-    }
-    else if (rand > 0.33 && rand < 0.77) {
-        compChoice = 'paper'
-        //return console.log(compChoice);
-    }
-    else {
-        compChoice = 'scissors'
-        //return console.log(compChoice)
-
-    }
-}
-
-function getHumanChoice () {
-    temp = prompt('enter your Move');
-    if (temp == 'rock' || temp == 'paper' || temp == 'scissors' ){
-        //onsole.log(temp);
-        humanChoice = temp;
-    }
-    else {
-        console.log('enter allowed value in lower case')
-    }
-
-}
-
-
-
-function playRound(humanChoice,compChoice)
-{
+for (let i = 0; i < 5; i++) {
     getCompChoice()
     getHumanChoice()
-        if (humanChoice == 'rock'){
-            if (compChoice == 'rock') {
+    playRound(humanChoice,compChoice)
+        
+}
+
+displayScore(humanScore,compScore,draw)
+
+function playRound(a,b) // a is human choice b is comp choice
+{
+        if (a == 'rock'){
+            if (b == 'rock') {
                 console.log('Match Draw');
                 draw = draw + 1;
                 }
-            if (compChoice == 'paper') {
+            if (b == 'paper') {
                 console.log('Paper beats Rock you lose');
                 compScore = compScore + 1;
             }
-            if (compChoice == 'scissors') {
+            if (b == 'scissors') {
                 console.log('Rock beats Scissors you win')
                 humanScore = humanScore +1;
-
             }
-
         }
-        if (humanChoice == 'paper') {
-            if (compChoice == 'paper') {
+        if (a == 'paper') {
+            if (b == 'paper') {
                 console.log('Match Draw');
                 draw = draw + 1;
-
                 }
-            if (compChoice == 'rock') {
+            if (b == 'rock') {
                 console.log('Paper beats Rock you win');
                 humanScore = humanScore +1;
-
             }
-            if (compChoice == 'scissors') {
+            if (b == 'scissors') {
                 console.log('Scissors beats paper you lose')
                 compScore = compScore + 1;
             }
-
         }
-        if (humanChoice == 'scissors') {
-            if (compChoice == 'rock') {
+        if (a == 'scissors') {
+            if (b == 'rock') {
                 console.log('Match Draw');
                 draw = draw + 1;
-
                 }
-            if (compChoice == 'paper') {
+            if (b == 'paper') {
                 console.log('Paper beats Scissors you win');
                 humanScore = humanScore +1;
             }
-            if (compChoice == 'rock') {
+            if (b == 'rock') {
                 console.log('Rock beats Scissors you lose')
                 compScore = compScore + 1;
             }
-
         }
 }  
 
@@ -108,6 +63,35 @@ function displayScore (a,b,c) {
     
     
 }
+
+function getCompChoice (){
+    let rand = Math.random();
+    if (rand > 0 && rand < 0.33){
+        compChoice = 'rock'
+        console.log('comp choice '+compChoice);
+    }
+    else if (rand > 0.33 && rand < 0.77) {
+        compChoice = 'paper'
+         console.log('comp choice '+compChoice);
+    }
+    else {
+        compChoice = 'scissors'
+         console.log('comp choice '+compChoice);
+    }
+}
+
+function getHumanChoice () {
+    temp = prompt('enter your Move');
+    if (temp == 'rock' || temp == 'paper' || temp == 'scissors' ){
+        humanChoice = temp;
+        console.log('human choice ' + humanChoice );
+                        
+    }
+    else {
+        console.log('enter allowed value in lower case')
+    }
+}
+
 
 
 
---
+++ temp debugginh/script.js
@@ -0,0 +1,2 @@
+console.log('enter here');
+
