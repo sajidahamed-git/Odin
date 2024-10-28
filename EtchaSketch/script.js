36c427b4d06fe2344fec14993feaf23161fe86d9
Sajid <sajidahamed2001@gmail.com>
2024-10-29 01:43:46 +0530

    grid generation working

--- EtchaSketch/script.js
+++ EtchaSketch/script.js
@@ -1,11 +1,26 @@
-let i = 5;
-let j = 2;
+function generatecontainerhtml() {
+  const container = document.querySelector(".container");
+  const rows = Number(document.querySelector(".input").value);
+console.log(typeof(rows));
+  if (rows <= 100 && Number.isInteger(rows)) {
+    console.log(rows);
+    const oneboxHTML = '<div class="boxes"></div>';
+    let containerHTML = "";
+    for (let index = 0; index < rows * rows; index++) {
+      containerHTML += oneboxHTML;
+    }
+    container.innerHTML = containerHTML;
+    const boxsizewidth = 1500 / rows; // depends upon max width of the container
+    const boxsizeheight = 600 / rows;
 
-let boxesHTML = "";
+    const boxes = document.querySelectorAll(".boxes");
 
-const onebox = '<div class="box"></div>';
-for (let index = 0; index < 255; index++) {
-  boxesHTML += onebox;
+    boxes.forEach((box) => {
+      box.style.width = `${boxsizewidth}px`;
+      box.style.height = `${boxsizeheight}px`;
+    });
+  } else document.querySelector(".container").innerHTML = "enter a integer number below 100";
+
+
+  
 }
-
-document.querySelector(".grid").innerHTML = boxesHTML;
