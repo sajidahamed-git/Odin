c024d2d68c146954bb60c0e08098f97ddff084e3
Sajid <sajidahamed2001@gmail.com>
2024-10-29 02:35:34 +0530

    temp files removed

--- temp dom/index.js
+++
@@ -1,8 +0,0 @@
-
-//let container = document.querySelector('#container')
-//let display = container.lastElementChild
-//display.innerHTML = 'display'
-
-
-document.querySelector("#container").lastElementChild.innerHTML= 'display element div'
-document.querySelector('#container').firstElementChild.innerHTML = ' control div e'
