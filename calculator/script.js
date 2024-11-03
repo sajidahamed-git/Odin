0558c1e0059e1a0af22bfc727c3815c6858a8be5
Sajid <sajidahamed2001@gmail.com>
2024-11-04 00:43:43 +0530

    found a way to add all buttons to variables in a better way

--- calculator/script.js
+++ calculator/script.js
@@ -1,1 +1,36 @@
-console.log('game game');
+const input = document.querySelector('.input')
+
+// const one = document.querySelector(".one");
+// const two = document.querySelector('.two')
+// const three = document.querySelector('.three')
+// const four = document.querySelector(".four");
+// const five = document.querySelector(".five");
+// const six = document.querySelector(".six");
+// const seven = document.querySelector(".seven");
+// const eight = document.querySelector(".eight");
+// const nine = document.querySelector(".nine");
+
+const allbuttons = document.querySelectorAll('.button')
+
+allbuttons.forEach(button =>{
+    button.addEventListener('click',()=>{
+        let html = input.textContent
+        input.textContent= html + button.textContent
+    })
+})
+
+// three.onclick = ()=>addinput(three)
+
+// function addinput (button){
+//     let html = input.innerHTML
+//     input.innerHTML = html + button.innerHTML
+// }
+
+// one.addEventListener('click',()=> {
+//     let html = input.innerHTML
+//     input.innerHTML = html + one.innerHTML
+// })
+// seven.addEventListener('click',()=> {
+//     let html = input.innerHTML
+//     input.innerHTML = html+seven.innerHTML
+//     })
