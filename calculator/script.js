751f0af8e6c5207778f9784867d5dbe951d2f386
Sajid <sajidahamed2001@gmail.com>
2024-11-04 17:59:32 +0530

    backspace functionality added

--- calculator/script.js
+++ calculator/script.js
@@ -1,36 +1,18 @@
-const input = document.querySelector('.input')
+const input = document.querySelector(".input");
 
-// const one = document.querySelector(".one");
-// const two = document.querySelector('.two')
-// const three = document.querySelector('.three')
-// const four = document.querySelector(".four");
-// const five = document.querySelector(".five");
-// const six = document.querySelector(".six");
-// const seven = document.querySelector(".seven");
-// const eight = document.querySelector(".eight");
-// const nine = document.querySelector(".nine");
+const allbuttons = document.querySelectorAll(".button");
+let inputText = "";
 
-const allbuttons = document.querySelectorAll('.button')
+allbuttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    inputText = inputText + button.innerHTML;
+    input.textContent = inputText;
+  });
+});
 
-allbuttons.forEach(button =>{
-    button.addEventListener('click',()=>{
-        let html = input.textContent
-        input.textContent= html + button.textContent
-    })
-})
+const backspace = document.querySelector(".backspace");
 
-// three.onclick = ()=>addinput(three)
-
-// function addinput (button){
-//     let html = input.innerHTML
-//     input.innerHTML = html + button.innerHTML
-// }
-
-// one.addEventListener('click',()=> {
-//     let html = input.innerHTML
-//     input.innerHTML = html + one.innerHTML
-// })
-// seven.addEventListener('click',()=> {
-//     let html = input.innerHTML
-//     input.innerHTML = html+seven.innerHTML
-//     })
+backspace.addEventListener("click", () => {
+  inputText = inputText.slice(0, -1);
+  input.textContent = inputText;
+});
