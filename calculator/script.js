edad9a7ba5bb6f4e8fa687cacce4be661a27942a
Sajid <sajidahamed2001@gmail.com>
2024-11-04 18:02:16 +0530

    cancel key functinality addee

--- calculator/script.js
+++ calculator/script.js
@@ -16,3 +16,11 @@ backspace.addEventListener("click", () => {
   inputText = inputText.slice(0, -1);
   input.textContent = inputText;
 });
+
+const cancel = document.querySelector('.cancel-button')
+
+cancel.addEventListener('click',()=>{
+    inputText = ''
+    input.textContent = inputText
+
+})
