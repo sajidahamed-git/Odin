6413a05ec2a28690e3c8eabc586e07d0a30e2ed3
Sajid <sajidahamed2001@gmail.com>
2024-11-05 14:15:36 +0530

    max limit added and equal functionality using eval()

--- calculator/script.js
+++ calculator/script.js
@@ -1,26 +1,37 @@
 const input = document.querySelector(".input");
-
 const allbuttons = document.querySelectorAll(".button");
+const backspace = document.querySelector(".backspace");
+const cancel = document.querySelector(".cancel-button");
+const result = document.querySelector(".result");
+
 let inputText = "";
 
 allbuttons.forEach((button) => {
   button.addEventListener("click", () => {
+    if (button.innerHTML === "=") {
+      calculate(inputText);
+      return;
+    }
     inputText = inputText + button.innerHTML;
+    if (inputText.length > 10) {
+      result.innerHTML = "max digits";
+      return;
+    }
     input.textContent = inputText;
   });
 });
-
-const backspace = document.querySelector(".backspace");
 
 backspace.addEventListener("click", () => {
   inputText = inputText.slice(0, -1);
   input.textContent = inputText;
 });
 
-const cancel = document.querySelector('.cancel-button')
-
-cancel.addEventListener('click',()=>{
-    inputText = ''
-    input.textContent = inputText
-
-})
+cancel.addEventListener("click", () => {
+  inputText = "";
+  input.textContent = inputText;
+  result.innerHTML = "";
+});
+function calculate(inputText) {
+  console.log(inputText);
+  result.innerHTML = eval(inputText);
+}
